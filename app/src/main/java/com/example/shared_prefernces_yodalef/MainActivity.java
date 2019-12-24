2477d9dbe16d3a77b91498c672ee0f0d45759bba
afasf <dd2640@bs.amalnet.k12.il>
2019-12-24 21:45:52 +0200

    afafa

--- app/src/main/java/com/example/shared_prefernces_yodalef/MainActivity.java
+++ app/src/main/java/com/example/shared_prefernces_yodalef/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.shared_prefernces_yodalef;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
     EditText et;
     String st;
+
+    /**
+     * @since 11/12/2019
+     * The onCreate check the last data the user entered
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,16 +40,28 @@ public class MainActivity extends AppCompatActivity {
         tv.setText("The number of clicks: "+count);
     }
 
+    /**
+     * @since 11/12/2019
+     * The method is onClick that count the clicks of the user on the button
+     */
     public void lets_Count(View view) {
         count++;
         tv.setText("The number of clicks: "+count);
     }
 
+    /**
+     * @since 11/12/2019
+     * The method reset the number of clicks the user did.
+     */
     public void Reset(View view) {
         count=0;
         tv.setText("The number of clicks: "+count);
     }
 
+    /**
+     * @since 11/12/2019
+     * The method close the application and save the data of the user
+     */
     public void End_App(View view) {
         st=et.getText().toString();
         SharedPreferences number = getSharedPreferences("count",MODE_PRIVATE);
@@ -49,5 +70,18 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("Word",st);
         editor.commit();
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent si = new Intent(this,Credits.class);
+        startActivity(si);
+        return true;
     }
 }
